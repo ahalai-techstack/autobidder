aadc6f9fe04ad4e2c6932b5912216231e7581866
Andrii Halai <andrii.halai@tech-stack.io>
2025-06-09 19:25:31 +0300

    Add registration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { LotModule } from './modules/lot/lot.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { AppService } from './app.service';
     }),
     UserModule,
     LotModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
