081e0fd3b8e6a8a1ece72c5c76d9c24866e0a8ff
Andrii Halai <andrii.halai@tech-stack.io>
2025-07-09 13:51:18 +0300

    Revert "Separate configs for dev and prod envs"
    This reverts commit 09ddfcfc4e1dd8dccba88dd8b7f478acd05f5032.

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,6 @@ import { CarModule } from './modules/car/car.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV ?? 'development'}`,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
