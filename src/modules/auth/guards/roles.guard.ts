88df95a7e616005009998a1a436d1a6d5eebdd9c
Andrii Halai <andrii.halai@tech-stack.io>
2025-08-06 18:41:26 +0300

    Throw error if user not found in request

--- src/modules/auth/guards/roles.guard.ts
+++ src/modules/auth/guards/roles.guard.ts
@@ -20,7 +20,7 @@ export class RolesGuard implements CanActivate {
     const user = request.user;
 
     if (!user) {
-      return false;
+      throw new Error('User not found in request');
     }
 
     return !!requiredRoles.find((role) => role === user?.role);
